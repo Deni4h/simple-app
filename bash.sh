b13545c45f7a728f9b1614b027e4f3d15fdde570
deni4h <denisatria4734@gmail.com>
2025-07-23 04:47:50 +0000

    deploy

--- bash.sh
+++ bash.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-git init
 git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:Deni4h/simple-app.git
-git push -u origin main
+git commit -m "deploy"
+git push origin main
